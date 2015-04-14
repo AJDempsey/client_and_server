cc9691f694f9b709276145a1a2ab6c5374e81db7
Anthony James Dempsey <anthonyjdempsey@gmail.com>
2015-04-14 20:08:58 +0100

    Adding basic hello world program to test CLion set up

---
+++ main.c
@@ -0,0 +1,12 @@
+//
+// Created by anthony on 14/04/15.
+//
+
+#include <stdio.h>
+#include "main.h"
+
+int main(char args[], int argv) {
+    printf(HELLO_STRING);
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+//
+// Created by anthony on 14/04/15.
+//
+
+#ifndef CLIENT_AND_SERVER_MAIN_H
+#define CLIENT_AND_SERVER_MAIN_H
+
+#endif //CLIENT_AND_SERVER_MAIN_H
+
+#define HELLO_STRING "Hello world!"
